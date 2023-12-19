2eadd09817aef0b56da485288abfec05a0a1a30a
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-19 01:56:57 -0300

    feito models cor

--- backend/elitemotors/admin.py
+++ backend/elitemotors/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Carro, Cor
+
+admin.site.register(Carro)
+admin.site.register(Cor)
+
 # Register your models here.
--- backend/elitemotors/models.py
+++ backend/elitemotors/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Cor(models.Model):
+    descricao: models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
+class Carro(models.Model):
+    modelo = models.CharField(max_length=100)
+ 
+    def __str__(self):
+        return self.modelo
 # Create your models here.

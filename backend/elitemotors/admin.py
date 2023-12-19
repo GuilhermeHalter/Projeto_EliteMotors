4ac3a03ab8720ea8a346d7900d1acbb35cfe6123
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-19 11:41:10 -0300

    feita tabela acessorio e cor

--- backend/elitemotors/admin.py
+++ backend/elitemotors/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Carro, Cor
+from .models import Carro, Cor, Acessorio
 
 admin.site.register(Carro)
 admin.site.register(Cor)
+admin.site.register(Acessorio)
+
 
 # Register your models here.
--- backend/elitemotors/models.py
+++ backend/elitemotors/models.py
@@ -6,9 +6,16 @@ class Cor(models.Model):
     def __str__(self):
         return self.descricao
 
+class Acessorio(models.Model):
+    descricao: models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+
 class Carro(models.Model):
     modelo = models.CharField(max_length=100)
- 
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="Carros")
+    acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name="Carros")
+
     def __str__(self):
         return self.modelo
-# Create your models here.

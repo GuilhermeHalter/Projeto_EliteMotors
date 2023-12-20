bc69ee5127c59850fb726fe74d5c7af86959e0ea
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-20 15:45:40 -0300

    fetio associação de imagem em carros

--- backend/elitemotors/models/carro.py
+++ backend/elitemotors/models/carro.py
@@ -1,10 +1,20 @@
 from django.db import models
 from elitemotors.models import Cor, Acessorio
+from uploader.models import Image
+
 
 class Carro(models.Model):
     modelo = models.CharField(max_length=100)
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="Carros")
     acessorio = models.ForeignKey(Acessorio, on_delete=models.PROTECT, related_name="Carros")
-
+    imagem = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
+    
     def __str__(self):
         return self.modelo
--- backend/elitemotors/serializers/carro.py
+++ backend/elitemotors/serializers/carro.py
@@ -1,8 +1,19 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 
 from elitemotors.models import Carro
 
 class CarroSerializer(ModelSerializer):
+    imagem_attachment_key = SlugRelatedField(
+        source="capa",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
+    
     class Meta:
         model = Carro
         fields = "__all__"
